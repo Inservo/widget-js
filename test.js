e0466902adfa7cc5508de9a40956e68b104c6283
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2025-03-24 15:47:07 +0100

    updateing test code to console

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var simpleDiv = document.createElement("div");
-  simpleDiv.textContent = "Hello";
-  document.body.appendChild(simpleDiv);
-});
+console.log("Hello Jsdeliver");
