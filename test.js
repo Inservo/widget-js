1e87d1827c2d5830507d2cc907001b0cca825477
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2025-03-24 18:17:12 +0100

    implementing button

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("Hello Jsdeliver");
+(function () {
+  // Create a button element
+  var button = document.createElement("button");
+  button.innerText = "Click Me";
+
+  // Attempt to insert the button after the current script tag
+  var currentScript = document.currentScript;
+  if (currentScript && currentScript.parentNode) {
+    currentScript.parentNode.insertBefore(button, currentScript.nextSibling);
+  } else {
+    // Fallback: Append to the body if currentScript isn't available
+    document.body.appendChild(button);
+  }
+})();
