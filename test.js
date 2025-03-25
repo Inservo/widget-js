97b2b67e72ddc4952963617c8c29551b3338fd77
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2025-03-25 15:02:34 +0100

    updating button style

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 (function () {
-  // Create a button element
   var button = document.createElement("button");
-  button.innerText = "Click Me";
+  button.innerText = "Book Now";
+
+  button.style.padding = "8px 12px";
+  button.style.backgroundColor = "#000000";
+  button.style.radius = "30px";
+  button.style.color = "#FFFFFF"
 
   // Attempt to insert the button after the current script tag
   var currentScript = document.currentScript;
