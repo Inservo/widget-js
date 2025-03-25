6e985d1548fadbddecaf74d3ba856e8f03fd2e16
Dmitry Filimonov <dmitryfilimonov333@gmail.com>
2025-03-25 15:18:35 +0100

    adding a radius and removing default settings for button

--- test.js
+++ test.js
@@ -2,9 +2,10 @@
   var button = document.createElement("button");
   button.innerText = "Book Now";
 
+  button.style.all = "unset"
   button.style.padding = "8px 12px";
   button.style.backgroundColor = "#000000";
-  button.style.radius = "30px";
+  button.style.radius = "50%";
   button.style.color = "#FFFFFF"
 
   // Attempt to insert the button after the current script tag
